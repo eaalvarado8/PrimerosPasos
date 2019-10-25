cf94cff7bac0d88b84a08d904b8e9c88d094dda9
eaalvarado8 <eric_alvarado2202@hotmail.com>
2019-10-24 20:09:22 -0500

    Practica en Clase

---
+++ PracticasEnClase/HolaEric.java
@@ -0,0 +1,7 @@
+public class HolaEric {
+	public static void main (String[] args) {
+	String nombre;
+	nombre = "Eric";
+	System.out.println("Saludos" + nombre + "!");
+	}
+}
---
+++ PracticasEnClase/Saludo2.java
@@ -0,0 +1,9 @@
+public class Saludo2 {
+	public static void main (String[] args) {
+		String nombre;
+		System.out.print("Por favor, dime cómo te llamas: ");
+		nombre = System.console().readLine();
+		System.out.print("Hola" + nombre);
+		System.out. println(", ¡encantado de concerte");
+	}
+}
